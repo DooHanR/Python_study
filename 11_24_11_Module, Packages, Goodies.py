98c63810abf06768649b6b5241416d421a518165
dhe <dhe_crysis@naver.com>
2022-11-25 10:25:39 +0900

    11/25, Module에 대한 본격적인 학습의 시작.

---
+++ 11_24_11_Module, Packages, Goodies.py
@@ -0,0 +1,21 @@
+"""
+ 지금부터 우리는 python의 완전한 program 을 작성할 것이다.
+python의 standard library을 사용하고 직접 Module을 만들어 볼 것이다.
+그렇게 해서 우리는 지금까지 처럼 단편적인 프로그램이 아니라
+제대로된 프로그램을 만들 수 있으리라 기대된다.
+"""
+
+"""
+Modules and the import Statement
+ Module 은 file 안에 있는 python 코드를 의미하며, 특별한 지정같은게 필요없이
+파일내의 Python code는 그자체로 다른 이들에게 import 되어 module로 사용될 수 있다.
+또한 Module 을 참조할때는 'import' statement 를 사용해서 해당 모듈 내의
+변수와 코드를 나의 프로그램에서 사용할 수 있게 해준다.
+"""
+
+"""
+Import a Module
+ import statement 를 사용하는 가장 간단한 방법은 import module이다.
+여기서는 랜덤으로 음식을 고르는 function 을 가지고 있는 모듈을 작성하고
+해당 모듈을 import 해보자.
+"""
