23beabfe820d24b49db3e751425b000a86186c78
dhe <dhe_crysis@naver.com>
2022-12-05 10:51:11 +0900

    12/05 본격적인 python 활용의 공부 시작. 지난주는 업무보조 하느라 너무 바뻐서 일을 하지를 못했다.

---
+++ 12_01_12_Wrangle_Mangle_Data.py
@@ -0,0 +1,15 @@
+"""
+ 여기서 부터 python 의 실질적인 실제 세계 프로그래밍에 들어갈 것이다.
+우린 여기서 먼저 data를 다루는 방법에 대해 공부 할 것인 데, data 와 관련해서
+data science 는 현재 아주 중요한 산업 분야 이기 때문에 알아두는게 좋다.
+ 먼저 기초적인 data format 에 대호 공부하고 data science를 한번 경험해보자.
+data format은 text, binary로 나뉘어 지는데 지금까지 스킵한 것들에 대해 배워 볼것이다.
+
+1. string
+- unicode character
+- regular expression patter matching
+
+2. binary
+- bytes for immutable eight-bit values
+- bytearrays for mutable ones.
+"""
