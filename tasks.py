8dd6ab5a27b1176dc02c780c2aa2196d4e93ecd9
dhe <dhe_crysis@naver.com>
2022-12-27 17:48:49 +0900

    12/27 실행시 문제가 마구잡이로 발생한다. 코드가 꼬였나?

--- tasks.py
+++ tasks.py
@@ -1,1 +1,11 @@
 """ invoke를 시행해보기 위해 만들어진 파일. """
+
+from invoke import task
+
+@task
+def mytime(ctx):
+    import time
+    now = time.time()
+    time_str = time.asctime(time.localtime(now))
+    print("Local time is", time_str)
+
