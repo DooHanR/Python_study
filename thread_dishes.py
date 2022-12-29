ed23c52c6cfe9d6bc47e3df7ee19f0953096891c
dhe <dhe_crysis@naver.com>
2022-12-29 15:44:57 +0900

    12/29 기존에 했던 process example을 thread로 redo 해보는 파일.

--- thread_dishes.py
+++ thread_dishes.py
@@ -1,1 +1,26 @@
-""" 앞서 했던 process-based dish example 을 thread를 이용해 다시 만들어보기."""
+""" 앞서 했던 process-based dish example 을 thread를 이용해 다시 만들어보기. """
+
+import threading, queue
+import time
+
+def washer(dishes, dish_queue):
+    for dish in dishes:
+        print ("Washing", dish)
+        time.sleep(5)
+        dish_queue.put(dish)
+
+def dryer(dish_queue):
+    while True:
+        dish = dish_queue.get()
+        print ("Drying", dish)
+        time.sleep(10)
+        dish_queue.task_done()
+
+dish_queue = queue.Queue()
+for n in range(2):
+    dryer_thread = threading.Thread(target=dryer, args=(dish_queue,))
+    dryer_thread.start()
+
+dishes = ['salad', 'bread', 'entree', 'dessert']
+washer(dishes, dish_queue)
+dish_queue.join()
